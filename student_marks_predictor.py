56d86200ee9b5ee693c7b51fd2815a3d67cdd1ca
Akhilesh Sharma <akhileshsharma@Akhileshs-MacBook-Air.local>
2026-07-04 11:13:26 +0530

    Add regression graph visualization

--- student_marks_predictor.py
+++ student_marks_predictor.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
 
 current_folder = Path(__file__).parent
 csv_file = current_folder / "student_marks.csv"
@@ -14,11 +16,26 @@ y = df["Marks"]
 print(X)
 print(y)
 
-from sklearn.linear_model import LinearRegression
+
 
 model = LinearRegression()
 
 model.fit(X, y)
+predictions = model.predict(X)
+
+plt.scatter(X, y, label="Actual Data")
+plt.plot(X, predictions, color="red", label="Regression Line")
+
+plt.xlabel("Hours Studied")
+plt.ylabel("Marks")
+plt.title("Linear Regression")
+
+plt.legend()
+plt.grid(True)
+
+plt.savefig("output.png")
+plt.show()
+
 prediction = model.predict([[9]])
 
 print(prediction)
